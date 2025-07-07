1d62cba68d0d77b94a5bc47c5f4c9c3622fac9c5
Jinyang Liu <liujinyang@bigtiger.me>
2025-07-07 16:26:31 +0800

    feat(theme): update Navbar/Logo component

--- src/theme/Navbar/Logo/index.tsx
+++ src/theme/Navbar/Logo/index.tsx
@@ -1,11 +1,11 @@
 import { type ReactNode } from 'react'
 import Logo from '@theme/Logo'
+import styles from './styles.module.css'
 
 export default function NavbarLogo(): ReactNode {
   return (
     <Logo
-      className="navbar__brand"
-      imageClassName="navbar__logo"
+      imageClassName={styles.navbarLogo}
       titleClassName="navbar__title text--truncate"
     />
   )
