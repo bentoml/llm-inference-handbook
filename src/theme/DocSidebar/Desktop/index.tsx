7741a6d514b8aca07c2b1267d2d2b823241c31c2
Jinyang Liu <liujinyang@bigtiger.me>
2025-07-09 01:16:00 +0800

    feat: customize DocSidebar/Desktop component

---
+++ src/theme/DocSidebar/Desktop/index.tsx
@@ -0,0 +1,39 @@
+import { memo } from 'react'
+import clsx from 'clsx'
+import { useThemeConfig } from '@docusaurus/theme-common'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import CollapseButton from '@theme/DocSidebar/Desktop/CollapseButton'
+import Content from '@theme/DocSidebar/Desktop/Content'
+import type { Props } from '@theme/DocSidebar/Desktop'
+
+import styles from './styles.module.css'
+
+function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }: Props) {
+  const {
+    navbar: { hideOnScroll },
+    docs: {
+      sidebar: { hideable }
+    }
+  } = useThemeConfig()
+  const {
+    siteConfig: { title }
+  } = useDocusaurusContext()
+
+  return (
+    <div
+      className={clsx(
+        styles.sidebar,
+        hideOnScroll && styles.sidebarWithHideableNavbar,
+        isHidden && styles.sidebarHidden
+      )}
+    >
+      <div className={styles.sidebarHeader}>
+        <h2>{title}</h2>
+      </div>
+      <Content path={path} sidebar={sidebar} />
+      {hideable && <CollapseButton onClick={onCollapse} />}
+    </div>
+  )
+}
+
+export default memo(DocSidebarDesktop)
