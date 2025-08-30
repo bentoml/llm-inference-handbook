3636a8e2bee5a926f1c73a8c6176f72e2de071b5
Jinyang Liu <liujinyang@bigtiger.me>
2025-08-30 20:07:19 +0800

    feat: support math equations

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import { themes as prismThemes } from 'prism-react-renderer'
 import type { Config } from '@docusaurus/types'
 import type * as Preset from '@docusaurus/preset-classic'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -46,6 +48,8 @@ const config: Config = {
         },
         docs: {
           routeBasePath: '/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           sidebarItemsGenerator: async (args) => {
             const { defaultSidebarItemsGenerator } = args
             const items = await defaultSidebarItemsGenerator(args)
@@ -67,7 +71,15 @@ const config: Config = {
       } satisfies Preset.Options
     ]
   ],
-
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous'
+    }
+  ],
   themeConfig: {
     colorMode: {
       disableSwitch: true
